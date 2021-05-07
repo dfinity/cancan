030ba244b0a0a9a7798c59f078a129c034542d90
Marco Segreto <marco.segreto@dfinity.org>
2021-05-06 17:58:43 -0700

    Makes correct check for when data is empty

--- src/components/RewardShowerNotification.tsx
+++ src/components/RewardShowerNotification.tsx
@@ -6,7 +6,7 @@ import LottieAnimation from "./LottieAnimation";
 import RewardPointAnim from "../assets/animations/RewardPointShower.json";
 import "./RewardShowerNotification.scss";
 
-const KEY_LOCALSTORAGE_READ_MESSAGES_TIME = 'ic-can-can-read_messages';
+const KEY_LOCALSTORAGE_READ_MESSAGES_TIME = "ic-can-can-read_messages";
 
 interface RewardShowerProps {
   currentUser: ProfileInfoPlus;
@@ -14,20 +14,23 @@ interface RewardShowerProps {
 
 // Count up all the reward points from a message
 function rewardPointsReducer(acc: number, message: Message) {
-  const { event } = message
+  const { event } = message;
   let reward = 0;
+
   // @ts-ignore
   if (event.uploadReward) reward = Number(event.uploadReward.rewards);
   // @ts-ignore
   if (event.superlikerReward) reward = Number(event.superlikerReward.rewards);
   // @ts-ignore
   if (event.transferReward) reward = Number(event.transferReward.rewards);
-  return acc + reward
+  return acc + reward;
 }
 
 // Filter messages based on the last date they were read, stored in localStorage
 function unreadMessagesFilter(message: Message) {
-  const lastReadTime = localStorage.getItem(KEY_LOCALSTORAGE_READ_MESSAGES_TIME);
+  const lastReadTime = localStorage.getItem(
+    KEY_LOCALSTORAGE_READ_MESSAGES_TIME
+  );
   if (!lastReadTime) return true;
   return message.time > BigInt(lastReadTime);
 }
@@ -40,6 +43,7 @@ export function RewardShowerNotification(props: RewardShowerProps) {
   function checkForNewMessages() {
     getMessages(props.currentUser.userName).then((messages) => {
       const unreadMessages = messages.filter(unreadMessagesFilter);
+      if (unreadMessages.length === 0) return;
       const rewardPoints = unreadMessages.reduce(rewardPointsReducer, 0);
       if (rewardPoints > 0) {
         setRewards(rewardPoints);
